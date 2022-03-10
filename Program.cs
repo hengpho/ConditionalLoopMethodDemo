9989db915d3d80f1665f6ea7150551f510a60218
Heng Pho <hengpho98@gmail.com>
2022-03-10 09:51:36 -0600

    Implement method to check if a number is in range of another number

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace ConditionalLoopMethodDemo
+{
+    class MainClass
+    {
+        static void Main()
+        {
+        
+
+        }
+
+        //access modifier - static (or not) - return type - name(params in here)
+        private static bool IsWithinRange(int num, int target, int range)
+        {
+            //make sure the return type is bool like we declared above, cannot return "hello" etc.
+            return (num - target) <= range;
+        }
+    }
+}
