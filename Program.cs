eab0a50d7dadb86064db751186e1142c5d0ccc76
Heng Pho <hengpho98@gmail.com>
2022-03-10 11:52:38 -0600

    Print a document has power, hasPaper and the ink level >= 10

--- Program.cs
+++ Program.cs
@@ -6,23 +6,6 @@ namespace ConditionalLoopMethodDemo
     {
         static void Main()
         {
-            // TODO: Ask the user for the initial number
-            // TODO: Ask the user for the target
-            // TODO: Ask the user for the range
-            // TODO: Write the result by calling IsWithinRange
-            Console.Write("What is the initial number: ");
-            int startingNum = int.Parse(Console.ReadLine());
-            Console.Write("What is the target: ");
-            int targetNum = int.Parse(Console.ReadLine());
-            Console.Write("What is the range: ");
-            int range = int.Parse(Console.ReadLine());
-
-            //turnary - if it meets requirements ? true do this : false do this;
-            string results = IsWithinRange(startingNum, targetNum, range) ? "Its within range." : "It's not within range.";
-
-            Console.WriteLine($"Given a starting number of: {startingNum} and a target number: {targetNum}. {results}");
-            //Console.WriteLine($"Given a starting number of: {startingNum} and a target number: {targetNum}. It's {IsWithinRange(startingNum, targetNum, range)}");
-
             Console.Write("Do we have power? (y/n): ");
             bool hasPower = Console.ReadLine().ToUpper() == "Y";
 
@@ -37,14 +20,6 @@ namespace ConditionalLoopMethodDemo
             
 
         }
-
-        //access modifier - static (or not) - return type - name(params in here)
-        private static bool IsWithinRange(int num, int target, int range)
-        {
-            //make sure the return type is bool like we declared above, cannot return "hello" which is a string etc.
-            return (num - target) <= range && (num - target) >= -range;
-        }
-
         //TODO: Print a document has Power, hasPaper and the ink level >= 10;
         private static void PrintDoc(bool hasPower, bool hasPaper, int inkLevel)
         {
